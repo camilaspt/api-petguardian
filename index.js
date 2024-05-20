60c4ecd51085118984f3d40197f9dd3e20802e43
camilaspt <camilaspitale26@gmail.com>
2024-05-19 22:10:27 -0300

    Agrego cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const express = require('express'); 
 const connection = require('./config/conectarDB.js');
+const cors = require('cors');
 const port = process.env.PORT || 3000;
 const usuarioRoutes = require('./routes/usuarioRoutes.js');
 
@@ -8,6 +9,9 @@ connection.conectarDB();
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:4200'
+}));
 app.use('/api/usuarios/', usuarioRoutes);
 
 app.listen(port, () => {
