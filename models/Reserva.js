0e57c5d0f13b6f2a54242b431ee442ae64518e2f
=Marina-Rookie <=mapanatti@gmail.com>
2024-08-13 20:48:12 -0300

    Update modelo Reserva

--- models/Reserva.js
+++ models/Reserva.js
@@ -1,39 +1,45 @@
-const mongoose = require ('mongoose');
-const ReservaSchema = new mongoose.Schema ({
-  fecha : {
-    type : Date,
-    default: Date.now,
-    trim : true 
+const mongoose = require("mongoose");
+const ReservaSchema = new mongoose.Schema({
+  fechaInicio: {
+    type: Date,
+    required: true,
+    trim: true,
+  },
+  fechaFin: {
+    type: Date,
+    required: true,
+    trim: true,
   },
 
-  tarifaTurno : {
-    type : Number,
-    required : true,
-    trim : true
+  tarifaTurno: {
+    type: Number,
+    required: true,
+    trim: true,
   },
 
-  comentario : {
-    type : String,
-    trim : true
+  comentario: {
+    type: String,
+    trim: true,
   },
 
   usuario: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Usuario',
-    required: true
+    ref: "Usuario",
+    required: true,
   },
 
-  mascotas: [{
+  mascotas: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Mascota",
+      required: true,
+    },
+  ],
+  estado: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Mascota',
-    required: true
-  }], 
-    estado: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Estado',
-    required: true
+    ref: "Estado",
+    required: true,
   },
-
 });
 
-module.exports = mongoose.model('Reserva', ReservaSchema);
+module.exports = mongoose.model("Reserva", ReservaSchema);
