14040c2a56d9034662f04c7f0111ab1b179dc27b
emadelbianco <emanuel.delbianco@gmail.com>
2024-09-25 19:32:02 -0300

    agrego logica disponibilidad cuidador

--- controllers/disponibilidadCuidadorController.js
+++ controllers/disponibilidadCuidadorController.js
@@ -1,8 +1,11 @@
 const Disponibilidad = require('../models/DisponibilidadCuidador.js');
+const Cuidador = require('../models/Usuario.js');
+const service = require('../services/disponibilidadCuidadorService.js');
 
-const getDisponibilidad = async (req, res) => {
+const getDisponibilidadPorCuidador = async (req, res) => {
     try { 
-        const disponibilidad = await Disponibilidad.find();
+        const idCuidador = req.params.idCuidador;
+        const disponibilidad = await Disponibilidad.find({cuidador: idCuidador});
         res.status(200).json(disponibilidad);
     } catch (error) {
         res.status(400).json({ message: error.message });
@@ -11,8 +14,7 @@ const getDisponibilidad = async (req, res) => {
 
 const createDisponibilidad = async (req, res) => {
     try {
-        const { inicio, fin } = req.body;
-        const newDisponibilidad = await Disponibilidad.create({inicio, fin});
+        const newDisponibilidad = await service.crearDisponibilidad(req.body);
         res.status(201).json(newDisponibilidad);
     } catch (error) {
         console.log(error.message);
@@ -22,8 +24,12 @@ const createDisponibilidad = async (req, res) => {
 
 const deleteDisponibilidad = async (req, res) => {
     try {
-        const idDisponibilidad = req.params.id;
-        const result = await Disponibilidad.deleteOne({_id:idDisponibilidad});
+        const idCuidador = req.params.idCuidador;
+        const dia = req.params.dia;
+        const result = await Disponibilidad.deleteMany({cuidador: idCuidador, dia: dia});
+        if (result.deletedCount === 0) {
+            return res.status(404).json({ message: 'No se encontraron disponibilidades para eliminar' });
+        }
         res.status(200).json(result); 
     } catch (error) {
         res.status(400).json({ message: error.message });
@@ -31,7 +37,7 @@ const deleteDisponibilidad = async (req, res) => {
 }
 
 module.exports = {
-    getDisponibilidad,
+    getDisponibilidadPorCuidador,
     createDisponibilidad,
     deleteDisponibilidad
 }
---
+++ services/disponibilidadCuidadorService.js
@@ -0,0 +1,31 @@
+const DisponibilidadCuidador = require('../models/DisponibilidadCuidador.js');
+
+const crearDisponibilidad = async (disponibilidad) => {
+    try {
+        const { dia, horaInicio, horaFin, cuidador } = disponibilidad;
+//verifico que para ese dia ese cuidador no tenga disponibilidad.
+        const disponibilidadExistente = await DisponibilidadCuidador.findOne({ dia, cuidador });
+        if (disponibilidadExistente) {
+            throw new Error('Ya existe una disponibilidad para el cuidador en el día indicado');
+        }
+//convierto en tipo Date las horas de inicio y fin, creo un arreglo con los turnos que vamos a crear y luego los inserto en la base de datos pero como string.
+        const inicio = new Date(`1970-01-01T${horaInicio}:00.000Z`);
+        const fin = new Date(`1970-01-01T${horaFin}:00.000Z`);
+        const disponibilidades = [];
+        for (let hora = inicio; hora < fin; hora.setHours(hora.getHours() + 1)) {
+            disponibilidades.push({
+                dia,
+                horaInicio: hora.toISOString().substring(11, 16),
+                horaFin: new Date(hora.getTime() + 60 * 60 * 1000).toISOString().substring(11, 16),
+                cuidador
+            });
+        }
+//inserto los turnos en la base de datos.
+        const newDisponibilidades = await DisponibilidadCuidador.insertMany(disponibilidades);
+        return newDisponibilidades;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
+
+module.exports = { crearDisponibilidad };
