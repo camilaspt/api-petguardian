fdc2cabb46ee85f49d6c4953f164635b8e6475d0
=Marina-Rookie <=mapanatti@gmail.com>
2024-09-27 23:18:54 -0300

    agrego campo para cuidador

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,69 +1,84 @@
 const moongose = require("mongoose");
-const UsuarioSchema = new moongose.Schema({
-  nombre: {
-    type: String,
-    required: true,
-    trim: true,
+const UsuarioSchema = new moongose.Schema(
+  {
+    nombre: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    apellido: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    telefono: {
+      type: String,
+      required: false,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    domicilio: {
+      type: String,
+      required: false,
+      trim: true,
+    },
+    rol: {
+      type: String,
+      required: true,
+      enum: [
+        "Cuidador Habilitado",
+        "Cuidador No Habilitado",
+        "Cuidador Pendiente",
+        "Cliente",
+        "Administrador",
+      ],
+      trim: true,
+    },
+    contactoEmergencia: {
+      //Campo para cliente
+      type: Number,
+      required: false,
+      trim: true,
+    },
+    nombreContactoEmergencia: {
+      //Campo para cliente
+      type: String,
+      required: false,
+      trim: true,
+    },
+    descripcionPersonal: {
+      //Campo para cuidador
+      type: String,
+      required: false,
+      trim: true,
+    },
+    tarifaHora: {
+      //Campo para cuidador
+      type: Number,
+      required: false,
+      trim: true,
+    },
+    promedioPuntuacion: {
+      //Campo para cuidador
+      type: Number,
+      required: false,
+      trim: true,
+    },
+    eliminado: {
+      type: Boolean,
+      default: false,
+    },
   },
-  apellido: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  telefono: {
-    type: String,
-    required: false,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  domicilio: {
-    type: String,
-    required: false,
-    trim: true,
-  },
-  rol: {
-    type: String,
-    required: true,
-    enum: ["Cuidador Habilitado", "Cuidador No Habilitado", "Cuidador Pendiente", "Cliente", "Administrador"],
-    trim: true,
-  },
-  contactoEmergencia: {
-    //Campo para cliente
-    type: Number,
-    required: false,
-    trim: true,
-  },
-  nombreContactoEmergencia: {
-    //Campo para cliente
-    type: String,
-    required: false,
-    trim: true,
-  },
-  descripcionPersonal: {
-    //Campo para cuidador
-    type: String,
-    required: false,
-    trim: true,
-  },
-  tarifaHora: {
-    //Campo para cuidador
-    type: Number,
-    required: false,
-    trim: true,
-  },
-  eliminado: {
-        type: Boolean,
-        default: false
-    }
-}, { timestamps: true }); 
+  { timestamps: true }
+); 
 
 module.exports = moongose.model("Usuario", UsuarioSchema);
