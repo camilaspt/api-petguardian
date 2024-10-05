d770a03018a07fa7371487b6a64fbc73cef9696d
=Marina-Rookie <=mapanatti@gmail.com>
2024-10-05 13:44:41 -0300

    creación de getDisponibilidadCuidador, se agrego el paquete moment-timezone para el manejo de zona horaria. Se agrego al creación de turnos en cxreateReserva

--- controllers/turnoController.js
+++ controllers/turnoController.js
@@ -1,37 +1,57 @@
-const Turno = require('../models/Turno.js');
+const turnoService = require("../services/turnoService.js");
+const Turno = require("../models/Turno.js");
 
 const getTurnos = async (req, res) => {
-    try { 
-        const turno = await Turno.find();
-        res.status(200).json(turno);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-}
+  try {
+    const turno = await Turno.find();
+    res.status(200).json(turno);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
 const createTurno = async (req, res) => {
-    try {
-        const { fechaHoraInicio } = req.body;
-        const newTurno = await Turno.create({fechaHoraInicio});
-        res.status(201).json(newTurno);
-    } catch (error) {
-        console.log(error.message);
-        res.status(400).json({ message: error.message });
-    }
-}
+  try {
+    const { fechaHoraInicio, reserva } = req.body;
+    const newTurno = await Turno.create({ fechaHoraInicio, reserva });
+    res.status(201).json(newTurno);
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({ message: error.message });
+  }
+};
 
 const deleteTurno = async (req, res) => {
-    try {
-        const idTurno = req.params.id;
-        const result = await Turno.deleteOne({_id:idTurno});
-        res.status(200).json(result); 
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-}
+  try {
+    const idTurno = req.params.id;
+    const result = await Turno.deleteOne({ _id: idTurno });
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+// Función para obtener la disponibilidad de un cuidador en un rango de fechas en zona horaria argentina
+  // Uso en el Front: cuando el cliente sellecciona el icono de reserva en la trajeta de cuidador y selecciona las fechas, este método te devuelve un array de horas disponibles,
+  // que se obtiene cruzando tanto la dispobilidad del cuidador como los turnos de las reservas que tiene el cuidador
+const getDisponibilidadCuidador = async (req, res) => {
+  try {
+    const { cuidadorId, fechaInicio, fechaFin } = req.body;
+
+    const disponibilidad = await turnoService.getDisponibilidadCuidador(
+      cuidadorId,
+      fechaInicio,
+      fechaFin
+    );
+    res.status(200).json(disponibilidad);
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({ message: error.message });
+  }
+};
 
 module.exports = {
-    getTurnos,
-    createTurno,
-    deleteTurno
-}
+  getTurnos,
+  createTurno,
+  deleteTurno,
+  getDisponibilidadCuidador,
+};
--- services/reservaService.js
+++ services/reservaService.js
@@ -1,8 +1,13 @@
-const Reserva = require("../models/Reserva");
+const moment = require("moment-timezone");
+const Reserva = require("../models/Reserva.js");
 const Turno = require("../models/Turno");
 const Usuario = require("../models/Usuario.js");
 const Estado = require("../models/Estado.js");
+const mongoose = require("mongoose");
 
+// createReserva: Crea unna reserva y sus turnos asociados
+  //no valida los turnos, eso se valida en la función getDisponibilidadCuidador de turnoService, el cliente seleccionaría directamente de ese array de horas disponibles
+  // Las reservas son con días consecutivos, se crea un turno de una hora para cada día al mimos horario. 
 const createReserva = async ({
   fechaInicio,
   fechaFin,
@@ -10,13 +15,15 @@ const createReserva = async ({
   clienteId,
   cuidador,
   mascotas,
+  horaTurno,
 }) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
   try {
     const estado = await Estado.findOne({ estado: "Pendiente" });
     if (!estado) {
       throw new Error("Estado 'Pendiente' no encontrado");
     }
-
     const cuidadorData = await Usuario.findById(cuidador);
     if (!cuidadorData || cuidadorData.rol !== "Cuidador Habilitado") {
       throw new Error("El cuidador no está habilitado");
@@ -34,13 +41,51 @@ const createReserva = async ({
       estado: estado._id,
     });
 
-    return await newReserva.save();
+    const savedReserva = await newReserva.save({ session });
+
+    // Calcular la cantidad de días entre fechaInicio y fechaFin
+    const startDate = new Date(fechaInicio);
+    const endDate = new Date(fechaFin);
+    const days = Math.ceil((endDate - startDate) / (1000 * 60 * 60 * 24));
+
+    // Crear turnos para cada día
+    for (let i = 0; i < days; i++) {
+      const turnoDate = new Date(startDate);
+      turnoDate.setDate(turnoDate.getDate() + i);
+      turnoDate.setHours(
+        parseInt(horaTurno.split(":")[0]),
+        parseInt(horaTurno.split(":")[1]),
+        0,
+        0
+      ); // Establecer la hora del turno
+
+      // Crear un nuevo turno
+      const newTurno = new Turno({
+        fechaHoraInicio: turnoDate,
+        reserva: savedReserva._id,
+      });
+      await newTurno.save({ session });
+    }
+
+    // Actualizar el contador de turnos en la reserva
+    const updatedReserva = await Reserva.findByIdAndUpdate(
+      savedReserva._id,
+      { $inc: { contadorTurnos: days } },
+      { new: true, session }
+    );
+
+    await session.commitTransaction();
+    session.endSession();
+
+    return updatedReserva;
   } catch (error) {
-    throw new Error("Error al crear la reserva: " + error.message);
+    await session.abortTransaction();
+    session.endSession();
+    throw error;
   }
 };
 
-// Función para obtener todas las reservas con sus turnos
+// Función para obtener todas las reservas con sus turnos, con time zone de argentina, en la BD esta guardado con +00:00 por defecto
 const getReservas = async () => {
   try {
     const reservas = await Reserva.find()
@@ -57,19 +102,59 @@ const getReservas = async () => {
         const precio = reserva.contadorTurnos * reserva.tarifaTurno;
         return {
           ...reserva.toObject(),
+          fechaInicio: moment(reserva.fechaInicio)
+            .tz("America/Argentina/Buenos_Aires")
+            .format(),
+          fechaFin: moment(reserva.fechaFin)
+            .tz("America/Argentina/Buenos_Aires")
+            .format(),
           precio,
-          turnos: turnos.map((turno) => turno.fechaHoraInicio),
+          turnos: turnos.map((turno) =>
+            moment(turno.fechaHoraInicio)
+              .tz("America/Argentina/Buenos_Aires")
+              .format()
+          ),
         };
       })
     );
-    
 
     return reservasConTurnos;
   } catch (error) {
     throw new Error(error.message);
   }
 };
+
+// Función para obtener las reservas de un cuidador en un rango de fechas que tengan estado Aprobada o Pendiente, se utiliza en la función getDisponibilidadCuidador de turnoService
+  // devuelve las reservas del cuidador en ese rango de fechas con estado Aprobada o Pendiente.
+const getReservasCuidadorEnRango = async (
+  idCuidador,
+  fechaInicio,
+  fechaFin
+) => {
+  try {
+    // Obtener los ObjectId de los estados "Aprobada" y "Pendiente"
+    const estados = await Estado.find({
+      estado: { $in: ["Aprobada", "Pendiente"] },
+    });
+    const estadoIds = estados.map((estado) => estado._id);
+    console.log("Estado IDs:", estadoIds);
+
+    const reservas = await Reserva.find({
+      cuidador: idCuidador,
+      fechaInicio: { $lte: fechaFin },
+      fechaFin: { $gte: fechaInicio },
+      estado: { $in: estadoIds },
+    });
+
+    return reservas;
+  } catch (error) {
+    throw new Error(
+      "Error al obtener las reservas del cuidador: " + error.message
+    );
+  }
+};
 module.exports = {
   getReservas,
   createReserva,
+  getReservasCuidadorEnRango,
 };
