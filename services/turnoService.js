ef2f561ad3d203778606e80ce8c007c248a7bb91
Marina.p <125839912+Marina-Rookie@users.noreply.github.com>
2025-02-12 00:05:29 -0300

    corrección horas getdisponibilidad

--- services/turnoService.js
+++ services/turnoService.js
@@ -7,7 +7,7 @@ const getDisponibilidadCuidador = async (cuidadorId, fechaInicio, fechaFin) => {
   try {
     const { getReservasCuidadorEnRango } = require("./reservaService.js");
 
-    // Extraer la fecha en formato YYYY-MM-DD
+    //  formato YYYY-MM-DD
     const fechaInicioSolo = fechaInicio.substring(0, 10);
     const fechaFinSolo = fechaFin.substring(0, 10);
 
@@ -16,14 +16,10 @@ const getDisponibilidadCuidador = async (cuidadorId, fechaInicio, fechaFin) => {
       .map(Number);
     const [endYear, endMonth, endDay] = fechaFinSolo.split("-").map(Number);
 
-    // Crear las fechas de inicio y fin del día en UTC
-    const startDate = new Date(Date.UTC(startYear, startMonth - 1, startDay));
-    startDate.setUTCHours(0, 0, 0, 0);
+    const startDate = new Date(startYear, startMonth - 1, startDay, 0, 0, 0, 0);
+    const endDate = new Date(endYear, endMonth - 1, endDay, 23, 59, 59, 999);
 
-    const endDate = new Date(Date.UTC(endYear, endMonth - 1, endDay));
-    endDate.setUTCHours(23, 59, 59, 999);
 
-    // Verificar que fechaInicio sea anterior a fechaFin
     if (startDate > endDate) {
       throw new Error("La fecha de inicio debe ser anterior a la fecha de fin");
     }
@@ -31,46 +27,43 @@ const getDisponibilidadCuidador = async (cuidadorId, fechaInicio, fechaFin) => {
     const disponibilidadPorFecha = {};
 
     // Iterar sobre cada día en el rango de fechas
-    for (
-      let date = new Date(startDate);
-      date <= endDate;
-      date.setUTCDate(date.getUTCDate() + 1)
-    ) {
-      const currentDate = new Date(date);
-      const startOfDay = new Date(currentDate);
-      startOfDay.setUTCHours(0, 0, 0, 0);
+ for (
+   let date = new Date(startDate);
+   date <= endDate;
+   date.setDate(date.getDate() + 1)
+ ) {
+   const currentDate = new Date(date);
+   const startOfDay = new Date(currentDate);
+   startOfDay.setHours(0, 0, 0, 0);
 
-      const endOfDay = new Date(currentDate);
-      endOfDay.setUTCHours(23, 59, 59, 999);
+   const endOfDay = new Date(currentDate);
+   endOfDay.setHours(23, 59, 59, 999);
 
-      const disponibilidad = await Disponibilidad.findOne({
-        cuidador: cuidadorId,
-        fecha: {
-          $gte: startOfDay,
-          $lte: endOfDay,
-        },
-      });
+   const disponibilidad = await Disponibilidad.findOne({
+     cuidador: cuidadorId,
+     fecha: {
+       $gte: startOfDay,
+       $lte: endOfDay,
+     },
+   });
 
-      if (disponibilidad) {
-        disponibilidadPorFecha[currentDate.toISOString().substring(0, 10)] =
-          disponibilidad.horas.map((hora) => parseInt(hora, 10));
-      } 
-    }
+   if (disponibilidad) {
+     disponibilidadPorFecha[currentDate.toISOString().substring(0, 10)] =
+       disponibilidad.horas.map((hora) => parseInt(hora, 10));
+   }
+ }
 
-
-    // Obtener las reservas del cuidador en el rango de fechas
+  
     const reservas = await getReservasCuidadorEnRango(
       cuidadorId,
       startDate,
       endDate
     );
 
-    // Obtener los turnos de las reservas
     const turnos = await Turno.find({
       reserva: { $in: reservas.map((r) => r._id) },
     });
-
-    // Verificar superposición de turnos
+    
     turnos.forEach((turno) => {
       const turnoFecha = moment(turno.fechaHoraInicio);
       const fecha = turnoFecha.format("YYYY-MM-DD");
