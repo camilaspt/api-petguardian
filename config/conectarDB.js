be143ec197444bc98a42c6c16f33dda0715517f7
emadelbianco <emanuel.delbianco@gmail.com>
2025-02-16 21:10:30 -0300

    fix log

--- config/conectarDB.js
+++ config/conectarDB.js
@@ -4,7 +4,6 @@ require("dotenv").config();
 // Conectar a MongoDB
 conectarDB = async () => {
   try {
-    console.log(process.env.MONGO_URI);
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Conectado a la base de datos");
   } catch (error) {
